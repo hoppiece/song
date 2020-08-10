2ed33fc62ce6ca76aafe03d17df5ce5b0132aeed
kenta.shinzato <kenta.shinzato@linecorp.com>
2020-08-10 14:45:48 +0900

    some fixes to perform mnist trial

--- song/mnist_trial.py
+++ song/mnist_trial.py
@@ -1,19 +1,40 @@
+import os.path
+import pickle
+
 import numpy as np
-from sklearn import datasets
 from matplotlib import pyplot as plt
+from sklearn import datasets
+from sklearn.preprocessing import StandardScaler
 
 from song_ import SONG
 
 DATA_PATH = "../data"
+MAX_DATA_NUM = 1000
+
+
+def loader():
+    dataset = "mnist_784"
+    try:
+        with open(os.path.join(DATA_PATH, dataset), "rb") as fp:
+            mnist = pickle.load(fp)
+    except FileNotFoundError:
+        mnist = datasets.fetch_openml(dataset, data_home=DATA_PATH)
+        with open(os.path.join(DATA_PATH, dataset), "wb") as fp:
+            pickle.dump(mnist, fp, -1)
+
+    print()
+    return mnist
 
 
 def main():
-    mnist = datasets.fetch_openml("mnist_784", data_home=DATA_PATH)
-    mnist_data = mnist["data"][:1000]
-    mnist_labels = mnist["target"][:1000]
+    mnist = loader()
+    stdsc = StandardScaler()
+    mnist_data = mnist["data"][:MAX_DATA_NUM]
+    mnist_labels = mnist["target"][:MAX_DATA_NUM]
+    mnist_data_std = stdsc.fit_transform(mnist_data)
     idx = []
     color = []
-    for i in range(1000):
+    for i in range(MAX_DATA_NUM):
         if mnist_labels[i] == "0":
             idx.append(i)
             color.append(0)
@@ -24,17 +45,14 @@ def main():
             idx.append(i)
             color.append(2)
 
-    print(len(idx), len(color))
     idx = np.array(idx)
 
     model = SONG()
-    model.fit(mnist_data, mnist_labels)
-    embedding = model.embeddings
+    model.fit(mnist_data_std[idx], color)
 
-    plt.scatter(embedding[:, 0], embedding[:, 1], c=model.embedding_label)
+    plt.scatter(model.raw_embeddings[:, 0], model.raw_embeddings[:, 1], c=color)
     plt.show()
 
 
 if __name__ == "__main__":
     main()
-
